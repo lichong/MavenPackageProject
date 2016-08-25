efa8f70d2492c0396b34bdb153ee6df13388317a
李冲 <lichong@users.noreply.github.com>
2016-08-25 23:33:23 +0800

    Add files via upload

---
+++ com.wnlc.too.maven/bin/start.bat
@@ -0,0 +1,1 @@
+java -jar Maven.jar
---
+++ com.wnlc.too.maven/src/main/java/com/wnlc/too/maven/main/Main.java
@@ -0,0 +1,15 @@
+package com.wnlc.too.maven.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main
+{
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args)
+	{
+		LOGGER.info("This is main class.");
+	}
+
+}
